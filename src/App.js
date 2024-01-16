5eb99999c76ed85e9b6ce7babecb356c41e83ac1
rveennes <robert.nesse@capgemini.com>
2024-01-16 10:38:47 +0100

    added react components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+import Nav from "./Nav";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Header>
+        <Nav />
+      </Header>
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,55 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <img alt="logo" src="Logo.svg"></img>
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/menu">Menu</a>
+          </li>
+          <li>
+            <a href="/reservations">Reservations</a>
+          </li>
+          <li>
+            <a href="/order">Order Online</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <a href="/home">Adress</a>
+          </li>
+          <li>
+            <a href="/about">Phone number</a>
+          </li>
+          <li>
+            <a href="/menu">Email</a>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <a href="/home">Adress</a>
+          </li>
+          <li>
+            <a href="/about">Phone number</a>
+          </li>
+          <li>
+            <a href="/menu">Email</a>
+          </li>
+        </ul>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
